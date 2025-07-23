b5ebb0831fc10ca7809ffcdef3c5390b026b71cb
Shubham366 <60032206+Shubham366@users.noreply.github.com>
2025-07-23 20:40:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import os
+import urllib.request
+import numpy as np
+from config import SessionLocal, Base, engine
+from save_products import save_products_in_chunks
+
+Base.metadata.create_all(bind=engine)
+
+#FETCHING_DATA
+url = "https://tyroo-engineering-assesments.s3.us-west-2.amazonaws.com/Tyroo-dummy-data.csv.gz"
+local_file = "Tyroo-dummy-data.csv.gz"
+
+pd.set_option('display.max_columns', None)
+
+try:
+    if not os.path.exists(local_file):
+        urllib.request.urlretrieve(url, local_file)
+except Exception as e:
+    print(f"Error downloading file: {e}")
+    exit(1)
+
+#READING_DATA AND CLEANING
+df = pd.read_csv(local_file, compression='gzip')
+print(df.columns)
+print("Before: Rows =", df.shape[0], ", Columns =", df.shape[1])
+
+df_unique = df.drop_duplicates()
+print("After: Rows =", df.shape[0], ", Columns =", df.shape[1])
+float_cols = [
+    'price', 'current_price', 'promotion_price',
+    'discount_percentage', 'rating_avg_value'
+]
+for col in float_cols:
+    df_unique[col] = pd.to_numeric(df_unique[col], errors='coerce')
+
+df_unique['availability'] = df_unique['availability'].astype(str)
+df_unique['availability'] = df_unique['availability'].map(
+    lambda x: '1' if str(x).lower() in ['in stock', 'yes', 'true', '1'] else '0'
+)
+df_unique['is_free_shipping'] = df_unique['is_free_shipping'].map(
+    lambda x: '1' if str(x).lower() in ['true', '1', 'yes'] else '0'
+)
+for col in df_unique.columns:
+    if pd.api.types.is_float_dtype(df_unique[col]):
+        mean_val = df_unique[col].mean()
+        df_unique[col].fillna(mean_val, inplace=True)
+    else:
+        mode = df_unique[col].mode()
+        if not mode.empty:
+            df_unique[col].fillna(mode[0], inplace=True)
+
+
+
+
+#SAVING_CLEANED_DATA INTO DATABASE
+save_products_in_chunks(df_unique, chunk_size=500)
+print("Data saved to database in chunks.")
+
---
+++ save_products.py
@@ -0,0 +1,48 @@
+from config import SessionLocal, Base, engine
+from schema.product_info import Product
+
+def save_products_in_chunks(df, chunk_size=500):
+    product_ids = set()
+    for start in range(0, len(df), chunk_size):
+        end = start + chunk_size
+        chunk = df.iloc[start:end]
+        session = SessionLocal()
+        products = []
+        for _, row in chunk.iterrows():
+            product_id = row['product_id']
+            if product_id in product_ids:
+                print(f"Skipping duplicate product_id: {product_id}")
+                continue
+            product_ids.add(product_id)
+            products.append(
+                Product(
+                    product_id=row['product_id'],
+                    sku_id=row['sku_id'],
+                    brand_name=row['brand_name'],
+                    product_url=row['product_url'],
+                    product_big_img=row['product_big_img'],
+                    description=row['description'],
+                    price=row['price'],
+                    current_price=row['current_price'],
+                    promotion_price=row['promotion_price'],
+                    discount_percentage=row['discount_percentage'],
+                    availability=row['availability'],
+                    is_free_shipping=row['is_free_shipping'],
+                    rating_avg_value=row['rating_avg_value'],
+                    venture_category_name_local=row['venture_category_name_local'],
+                    venture_category2_name_en=row['venture_category2_name_en'],
+                    seller_name=row['seller_name'],
+                    business_area=row['business_area'],
+                    business_type=row['business_type']
+                )
+            )
+        try:
+            session.add_all(products)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            print(f"Error saving chunk: {e}")
+        finally:
+            session.close()
+
+            
